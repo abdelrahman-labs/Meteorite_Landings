fcd04ccca3dea7336331a6c21c201317a567f306
AbdelRahman-labs <20386532+AbdelRahman-labs@users.noreply.github.com>
2023-06-27 22:22:47 +0300

    Update main.py

--- main.py
+++ main.py
@@ -90,6 +90,8 @@ elif page == 'Project 1: Meteorite Landings':
     continents = continents.rename(columns={'index': 'Continent Name', 'Continent Name': 'Count'})
     total_meteorites = continents['Count'].sum()
     # Convert 'Count' column to numeric values
+    st.write(continents)
+    st.write(total_meteorites)
     continents['Count'] = pd.to_numeric(continents['Count'])
 
     continents['Percentage'] = 100 * continents['Count'] / total_meteorites
